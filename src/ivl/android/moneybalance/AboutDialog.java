cef18eb0f85dc7a31f19ab7b96308c6dc75c1f6e
Djiko <djiko@djiko.net>
2016-05-13 17:16:44 +0200

    Adds version number and build in About Dialog Updates fr translation

--- src/ivl/android/moneybalance/AboutDialog.java
+++ src/ivl/android/moneybalance/AboutDialog.java
@@ -2,6 +2,9 @@ package ivl.android.moneybalance;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
@@ -27,6 +30,21 @@ class AboutDialog extends Dialog {
 
 		TextView license = (TextView) findViewById(R.id.about_licence_copy);
 		license.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView versionNumberView = (TextView) findViewById(R.id.about_version_number);
+        try {
+            Context ctx = getContext();
+            Resources res = ctx.getResources();
+            PackageInfo packageInfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            Integer versionBuild = packageInfo.versionCode;
+            String text = String.format(res.getString(R.string.about_version_number), res.getString(R.string.app_name), versionName, versionBuild);
+            versionNumberView.setText(text);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
 	}
 
 }
